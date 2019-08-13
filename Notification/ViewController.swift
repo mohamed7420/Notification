d6c76331a1c31642a7148185b5f974b697ff83ce
mohamedosama2074 <iosengks74@gmail.com>
2019-08-13 22:40:53 +0200

    Notification

--- Notification/ViewController.swift
+++ Notification/ViewController.swift
@@ -7,12 +7,56 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        
+        showNotification()
+    }
+    
+    func showNotification(){
+        
+        // Step1: request user permission
+        
+        let center = UNUserNotificationCenter.current()
+        
+        center.requestAuthorization(options: [.alert , .sound]) { (granted, error) in
+            
+        }
+        
+        // create content of notification
+        
+        let content = UNMutableNotificationContent()
+        
+        content.title = "You Have a new Notitification"
+        content.body = "Open your new notification"
+        
+        
+        // create trigger
+        
+        let date = Date().addingTimeInterval(5)
+        
+        let dateComponent = Calendar.current.dateComponents([.year , .month , .day , .hour , .minute , .second], from: date)
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponent, repeats: false)
+        
+        // create request
+        
+        let uuidString = UUID().uuidString
+        
+        let request = UNNotificationRequest.init(identifier: uuidString, content: content, trigger: trigger)
+        
+        // register with notification center
+        
+        center.add(request) { (error) in
+            
+            
+        }
+        
     }
 
 
